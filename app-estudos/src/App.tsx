7a9a925d61f4c368925a6b8e26083f81f9e5540e
Paulo Senna <65934431+Sennafox@users.noreply.github.com>
2022-05-22 22:51:42 -0400

    Adicionando ao github

---
+++ app-estudos/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Forms from './Components/Forms';
+import List from './Components/List';
+
+function App() {
+  return (
+    <div className="App">
+      <Forms/>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app-estudos/src/Components/Forms/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Button from '../Button';
+class Forms extends React.Component {
+    render (){
+        return(
+         <form>
+             <div>
+                <label htmlFor="tarefa">
+                </label>
+                <input 
+                type = "text"
+                name="tarefa"
+                id="tarefa"
+                placeholder="O que você quer estudar"
+                required/>    
+             </div>
+             <div>
+            
+                <label htmlFor="tempo">
+                </label>
+                <input 
+                type = "time"
+                step="1"
+                name="tempo"
+                id="tempo"
+                min="00:00:00"
+                max="01:30:00"
+                required/>
+             </div>
+             <div>
+                <Button/>
+             </div>
+         </form>   
+        )
+    }
+}
+
+export default Forms;
